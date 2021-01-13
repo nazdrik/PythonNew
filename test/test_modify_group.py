7c81eb56955707ea79947a5a864bbbbad385973f
nazdrik <nazdrachovs@gmail.com>
2021-01-13 20:41:56 +0200

    tests are improved

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,13 +2,21 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New Group Name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New Group header"))
-    app.session.logout()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(footer="New Group footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
